362d50ff9edd695fa0bf06c85608977456a186e0
sushmitharchouhansush <sushmitharchouhansushmitharcho@gmail.com>
2026-09-04 12:45:37 +0530

    Added txt

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // src/App.jsx
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar'; // Nsvbar: navigation bar
 import Footer from './components/Footer/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';
+import Home from './pages/Home'; // 1
+import About from './pages/About';// 2
+import Projects from './pages/Projects';// 3
+import Contact from './pages/Contact';// 4
+import NotFound from './pages/NotFound';// 5
 
 function App() {
   const [theme, setTheme] = useState('light');
